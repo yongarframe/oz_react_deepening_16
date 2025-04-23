32e91b9a9764683f9a34d4142ea7470ab05790ad
yongarframe <yongar2002@gmail.com>
2025-04-23 18:11:34 +0900

    수정버튼 클릭 시 input 창 기존값 유지

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-import { memo, useRef, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 
 const TodoItem = memo(({ id, text, completed, onToggle, onDelete, onEdit }) => {
     const [isEditing, setIsEditing] = useState(false);
@@ -11,13 +11,25 @@ const TodoItem = memo(({ id, text, completed, onToggle, onDelete, onEdit }) => {
         setIsEditing(false);
     };
 
+    // useEffect(() => {
+    //     if (isEditing && inputRef.current) {
+    //         inputRef.current.value = text;  // input 창에 text 값넣기 방법 1
+    //         inputRef.current.focus();
+    //     }
+    // }, [isEditing]);
+
     return (
         <div className={`flex items-center p-2.5 border-b border-gray-200 ${completed ? 'bg-gray-50' : ''}`}>
             <input type="checkbox" checked={completed} onChange={() => onToggle(id)} className="mr-2" />
 
             {isEditing ? (
                 <div className="flex flex-1">
-                    <input ref={inputRef} type="text" className="flex-1 mr-2 p-1.5 border border-gray-300 rounded" />
+                    <input
+                        ref={inputRef}
+                        defaultValue={text}
+                        type="text"
+                        className="flex-1 mr-2 p-1.5 border border-gray-300 rounded"
+                    />
                     <button onClick={handleEdit} className="px-2 py-1 bg-blue-500 text-white rounded mr-1">
                         저장
                     </button>
@@ -30,9 +42,9 @@ const TodoItem = memo(({ id, text, completed, onToggle, onDelete, onEdit }) => {
                     <span className={`flex-1 ml-2.5 ${completed ? 'line-through text-gray-500' : ''}`}>{text}</span>
                     <button
                         onClick={() => {
-                            setTimeout(() => {
-                                inputRef.current.value = text;
-                            }, 1);
+                            // setTimeout(() => {
+                            //     inputRef.current.value = text;
+                            // }, 1);
                             setIsEditing(true);
                         }}
                         className="px-2 py-1 bg-yellow-500 text-white rounded mr-1"
